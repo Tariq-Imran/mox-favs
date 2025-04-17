cb804419f39ebc3742aca2f52d220796f525a93c
web3space <152457396+Tariq-Imran@users.noreply.github.com>
2025-04-17 10:53:36 +0600

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,10 +1,16 @@
+
 from src import favorites
 
-def deploy():
+def deploy_favorites():
     favorites_contract = favorites.deploy()
     starting_number = favorites_contract.retrieve()
     print(f"Starting number is {starting_number}")
 
+    favorites_contract.store(77)
+    ending_number = favorites_contract.retrieve()
+    print(f"Ending number is : {ending_number}")
+    return favorites_contract
+
 
 def moccasin_main():
-    deploy()
+    deploy_favorites()
